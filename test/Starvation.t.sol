1428615c010280086bb456846c3a8e6a5f03424d
fredo <luehrs.fred@gmail.com>
2024-10-09 16:42:19 +0200

    test: add tests to measure gas

---
+++ test/Starvation.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {AssertionAdopter, Starvation} from "../src/Starvation.sol";
+
+contract StarvationTest is Test {
+    Starvation public starvation;
+
+    function setUp() public {
+        starvation = new Starvation();
+
+    }
+
+    function addAdopters(uint32 amount) internal {
+        for(uint32 i = 0; i < amount; i++){
+            AssertionAdopter adopter = new AssertionAdopter();
+            starvation.addAssertionAdopter(address(adopter));
+        }
+    }
+
+    function test_StarvationZero() public {
+        address(starvation).call("");
+    }
+
+    function test_StarvationOne() public {
+        addAdopters(1);
+        vm.resetGasMetering();
+        address(starvation).call("");
+    }
+
+    function test_StarvationTen() public {
+        addAdopters(10);
+        vm.resetGasMetering();
+        address(starvation).call("");
+    }
+
+    function test_StarvationHundred() public {
+        addAdopters(100);
+        vm.resetGasMetering();
+        address(starvation).call("");
+    }
+
+    function test_StarvationFiveHundred() public {
+        addAdopters(500);
+        vm.resetGasMetering();
+        address(starvation).call("");
+    }
+
+}
